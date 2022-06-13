2f711eff3b7d4768a894332b31ca4200f3be1055
Maksim Dimitrov <dimitrov.maksim@gmail.com>
2022-06-13 16:17:05 +0300

    Initial commit

---
+++ nft-auction/src/demo-nft.ts
@@ -0,0 +1,34 @@
+import { ipfs, json, Bytes } from '@graphprotocol/graph-ts';
+import { DemoNFT, Transfer } from "../generated/DemoNFT/DemoNFT";
+import { DemoTransfer, Demo } from "../generated/schema";
+
+export function handleTransfer(event: Transfer): void {
+  const id = event.params.tokenId.toHexString();
+  let demo = Demo.load(id);
+  if (!demo) {
+    demo = new Demo(id);
+    const contract = DemoNFT.bind(event.address);
+    const hash = contract.tokenURI(event.params.tokenId);
+    const data = ipfs.cat(hash);
+
+    if (data) {
+      const jsonData = json.fromBytes(data as Bytes).toObject();
+
+      const imageUrl = jsonData.get("imageUrl");
+
+      if (imageUrl) {
+        demo.imageUrl = imageUrl.toString();
+      }
+    }
+  }
+
+  const demoTransfer = new DemoTransfer(event.transaction.hash.toHexString());
+  demoTransfer.from = event.params.from;
+  demoTransfer.to = event.params.to;
+  demoTransfer.token = demo.id;
+  demoTransfer.save();
+
+  demo.transfers = new Array();
+  demo.transfers.push(event.transaction.hash.toHexString());
+  demo.save();
+}
